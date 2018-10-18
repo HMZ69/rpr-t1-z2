35093f0e5ef4f183f73b6a535d62d61fe02ef0bd
Hamza Muminović <44194330+HMZ69@users.noreply.github.com>
2018-10-18 16:07:09 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,24 @@
+package ba.unsa.etf.rpr;
+import java.util.Scanner;
+
+public class Main {
+
+    public static int sumaCifara(int n) {
+        int suma = 0;
+        while (n != 0) {
+            suma += n % 10;
+            n = n / 10;
+        }
+        return suma;
+    }
+
+    public static void main(String[] args) {
+        int n = 0;
+        System.out.printf("Unesite broj n: ");
+        Scanner ulaz = new Scanner(System.in);
+        n = ulaz.nextInt();
+        for (int i = 1; i <= n; i++)
+            if (i % sumaCifara(i) == 0)
+                System.out.println(i);
+    }
+}
